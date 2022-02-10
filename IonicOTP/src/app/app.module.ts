29160737c2d6d85852f041e923c78523ed0ab226
helenomatoss <helenovitor15@gmail.com>
2022-02-10 10:07:35 -0300

    TesteSms

--- IonicOTP/src/app/app.module.ts
+++ IonicOTP/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
+import { SmsRetriever } from '@awesome-cordova-plugins/sms-retriever/ngx';
 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
@@ -11,7 +12,7 @@ import { AppComponent } from './app.component';
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [SmsRetriever,{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- IonicOTP/src/app/tab1/tab1.page.ts
+++ IonicOTP/src/app/tab1/tab1.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SmsRetriever } from '@awesome-cordova-plugins/sms-retriever/ngx';
 
 @Component({
   selector: 'app-tab1',
@@ -6,11 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['tab1.page.scss']
 })
 export class Tab1Page implements OnInit {
+  hash: any;
 
-  constructor() {}
+  constructor(private smsRetriever: SmsRetriever) { }
   ngOnInit(): void {
       console.log("estou aqui");
       
+  }
+  genHash() {
+   
+    this.smsRetriever.getAppHash()
+      .then((res: any) => {
+        console.log(res);
+        alert(res);
+        this.hash = res;
+      })
+      .catch((error: any) => console.error(error));
+  }
+
+
+  retriveSMS() {
+    console.log('Watching SMS');
+    this.smsRetriever.startWatching()
+      .then((res: any) => {
+        console.log(res);
+        //  <#> 323741 is your 6 digit OTP for MyApp. LDQEGVDEvcl
+        const otp = res.Message.toString().substr(4, 6);
+        alert(`OTP Received - ${otp}`);
+      })
+      .catch((error: any) => console.error(error));
   }
 
 }
